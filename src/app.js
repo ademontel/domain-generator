6996090c1d93e557de043d0f25dbaa4006d5c9e8
ademontel <53986476+ademontel@users.noreply.github.com>
2024-09-18 00:57:48 +0000

    Se agregó visualización en index.html

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
+/*eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+/*
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+*/
+
+let mainArray = [];
+
+let pronouns = ["the", "our"];
+let adjs = ["great", "big", "last"];
+let nouns = ["jogger", "racoon", "of"];
+let tlds = [".com", ".me", ".ed", ".us"];
+
+pronouns.forEach(pronoun => {
+  adjs.forEach(adj => {
+    nouns.forEach(noun => {
+      tlds.forEach(tld => {
+        mainArray.push(`<li> ${pronoun}${adj}${noun}${tld} </li>`);
+      });
+    });
+  });
+});
+
+let list = (document.getElementById("domain-names").innerHTML = mainArray.join(
+  " "
+));
